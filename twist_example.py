53f9e38e58b5a03ab23a32c5b1f893c9a5f2ca25
Percy （Yuxuan）Chen <147556132+PercyChen451@users.noreply.github.com>
2025-04-13 12:48:08 -0700

    Add files via upload

--- twist_example.py
+++ twist_example.py
@@ -1,20 +1,23 @@
 """ Example of how to set attributes in ROS messages """
 from geometry_msgs.msg import Twist
-
-
 def twist_fill():
-    '''
-    Fill a twist message with non-zero values
-    '''
-    # This is a stub. Substitute with your own code
+    """Create and populate a Twist message with non-zero values.
+    Returns:
+        Twist: A Twist message with all fields set to non-zero floating point values
+    """
     msg = Twist()
-    # Set linear velocity arbitrary 
-    msg.linear.x = 1.5          
-    msg.linear.y = 0.2 
-    msg.linear.z = 0.1  
-    # Set angular velocity arbitrary 
-    msg.angular.x = 0.3  
-    msg.angular.y = 0.4 
-    msg.angular.z = 1.0 
-
+    # Set linear velocity with arbitrary values
+    msg.linear.x = 1.5
+    msg.linear.y = 0.2
+    msg.linear.z = 0.1
+    # Set linear velocity with arbitrary values
+    msg.angular.x = 0.3
+    msg.angular.y = 0.4
+    msg.angular.z = 1.0
     return msg
+
+
+if __name__ == '__main__':
+    """Test the twist_fill function when run as a script."""
+    twist_msg = twist_fill()
+    print("Generated Twist message:\n", twist_msg)
